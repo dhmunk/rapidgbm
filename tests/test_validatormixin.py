466899b0d9ab18be40b2c236cbc2adc3802f3220
Daniel Porsmose <95210463+dhmunk@users.noreply.github.com>
2024-03-24 12:48:47 +0100

    fix test + pypi test

--- tests/test_validatormixin.py
+++ tests/test_validatormixin.py
@@ -1,16 +1,16 @@
-from rapidgbm.utility.validatormixin import ValidationMixin
+from rapidgbm.utility.validatormixin import ArgValidationMixin
 
 def test_is_bool():
-    assert ValidationMixin._is_bool(True) == True
-    assert ValidationMixin._is_bool(False) == True
-    assert ValidationMixin._is_bool(1) == False
-    assert ValidationMixin._is_bool('hello world') == False
+    assert ArgValidationMixin._is_bool(True) == True
+    assert ArgValidationMixin._is_bool(False) == True
+    assert ArgValidationMixin._is_bool(1) == False
+    assert ArgValidationMixin._is_bool('hello world') == False
 
 def test_is_int():
-    assert ValidationMixin._is_int(1) == True
-    assert ValidationMixin._is_int(-1) == True
-    assert ValidationMixin._is_int(1.0) == False
-    assert ValidationMixin._is_int('hello world') == False
+    assert ArgValidationMixin._is_int(1) == True
+    assert ArgValidationMixin._is_int(-1) == True
+    assert ArgValidationMixin._is_int(1.0) == False
+    assert ArgValidationMixin._is_int('hello world') == False
 
 
 
